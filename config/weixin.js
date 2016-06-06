35045387299bf83675f75830e9945c41d556ebe7
shanelau <shanelau1021@gmail.com>
2016-06-06 11:03:01 +0800

    delete key

--- config/weixin.js
+++ config/weixin.js
@@ -2,11 +2,11 @@
 
 module.exports.weixin = {
   'wxd98888751036c960':{    // key  appid
-    secret: '6aa0925e117874335068d95c370880291',
+    secret: '',
     url: 'http://test.weixin.bigertech.com'
   },
   'wxb0def0bc73c04b72':{    // key  appid
-    secret: 'b4d0cce349009e82ec96797080aeb3e9',
+    secret: '',
     url: 'http://test.weixin.bigertech.com'
   },
   // 更多的 app
